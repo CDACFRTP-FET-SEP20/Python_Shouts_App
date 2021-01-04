0657e3210cbd0edf19560aa78c8f88fa4ef6ed8b
Abhishek <absingh@cybage.com>
2021-01-04 20:56:02 +0530

    profile update added

--- shouts/accounts/views.py
+++ shouts/accounts/views.py
@@ -15,41 +15,49 @@ import base64
 class ProfileViewSets(viewsets.ModelViewSet):
 
     serializer_class = ProfileSerializer
-    # parser_classes = (MultiPartJsonParsers, parsers.JSONParser)
     queryset = Profile.objects.all()
     
 
 class LoginViewSets(views.APIView):
 
+
+    #Handles Post Request containing email and password 
+    #Authenticates User by passing it to Login Serializer
     def post(self, request):
         print(request.data)
 
         serializer = LoginSerializer(data=request.data)
-        print("in views " , serializer)
+        
 
         # if user is present it will not affect but if not request will not proceed
         # but response will be send back from here only
         serializer.is_valid(raise_exception=True)
-        print("serializer.data",serializer.data)
+        
+
         user = serializer.validated_data["user"]
-        print("userType",type(user))
-        print("Final " , user)
-        login(request, user)
 
+        if user:
+            login(request, user)
 
-        # with open(user.user_image,'rb') as image_file:
-        #     image_data = base64.b64encode(image_file.read()).decode('utf-8')
+            image_data = base64.b64encode(user.user_image.read()).decode('utf-8')
+            
+            #To get token key of logged in User 
+            token = Token.objects.get(user=user)
+            return Response({"token" : token.key, "user_id" : user.user_id , "username":user.username,"email":user.email,"user_image":image_data,"bio":user.bio}, status=200)
 
-        image_data = base64.b64encode(user.user_image.read()).decode('utf-8')
+        else:
+            message = serializer.validated_data["message"]
+            return Response({"message" : message})
+        
         
 
-        token = Token.objects.get(user=user)
+
 
         
+        
 
-        return Response({"token" : token.key, "user_id" : user.user_id , "username":user.username,"email":user.email,"user_image":image_data,"bio":user.bio}, status=200)
-        # return Response({"token" : token.key, "user_id" : user.user_id }, status=200)
-
+        
+        
 
 
          
